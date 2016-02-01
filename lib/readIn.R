d444f80871a42de068c6988427072793f148d185
arnoldlcl <arnoldlindlau@gmail.com>
2016-01-31 19:57:03 -0500

    added code to incorporate survey weights

--- lib/readIn.R
+++ lib/readIn.R
@@ -9,13 +9,15 @@ library(data.table)
 #'pathB      : string / path of "ss13pusb.csv"
 #'
 
-readIn_pop = function(reRead, colsToKeep, pathA, pathB){
+readIn_pop = function(reRead, colsToKeep = NULL, pathA, pathB, save = TRUE){
   if(reRead == TRUE){
     popDataA = fread(pathA, select = colsToKeep)
     popDataB = fread(pathB, select = colsToKeep)
     popData = rbind(popDataA, popDataB)
     rm(popDataA, popDataB)
+    if(save == TRUE){
     save(popData, file = "popData.RData")
+    }
   }else{
     load("popData.RData")
   }
@@ -31,13 +33,15 @@ readIn_pop = function(reRead, colsToKeep, pathA, pathB){
 #'pathB      : string / path of "ss13husb.csv"
 #'
 
-readIn_house = function(reRead, colsToKeep, pathA, pathB){
+readIn_house = function(reRead, colsToKeep = NULL, pathA, pathB, save = TRUE){
   if(reRead == TRUE){
     houseDataA = fread(pathA, select = colsToKeep)
     houseDataB = fread(pathB, select = colsToKeep)
     houseData = rbind(houseDataA, houseDataB)
     rm(houseDataA, houseDataB)
+    if(save == TRUE){
     save(houseData, file = "houseData.RData")
+    }
   }else{
     load("houseData.RData")
   }
